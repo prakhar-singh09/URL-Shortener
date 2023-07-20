e63a78136b21b1c3018bf2e30c907ad72bb48274
Prakhar Singh <92270107+intensifyprakhar@users.noreply.github.com>
2023-07-21 01:22:38 +0530

    Update staticRouter.js

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -4,7 +4,7 @@ const URL = require("../models/url");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  if (!req.user) return res.redirect("/");
+  if (!req.user) return res.redirect("/login");
   const allurls = await URL.find({ createdBy: req.user._id });
   return res.render("home", {
     urls: allurls,
